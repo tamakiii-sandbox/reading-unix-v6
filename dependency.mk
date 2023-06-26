5b2730d22307fbf3b055f3cdd6a770f99cba2cef
tamakiii <tamakiii@users.noreply.github.com>
2023-06-26 19:25:09 +0900

    add dependency/simh.trailing-edge.com/uv6swre

--- dependency.mk
+++ dependency.mk
@@ -5,7 +5,8 @@ help:
 
 build: \
 	dependency/simh/simh \
-	dependency/memnoth/unix-v6
+	dependency/memnoth/unix-v6 \
+	dependency/simh.trailing-edge.com/uv6swre
 
 clean:
 	rm -rf dependency
@@ -16,10 +17,19 @@ dependency/simh/simh: dependency/simh
 dependency/memnoth/unix-v6: dependency/memnoth
 	test -d $@ && git -C $@ pull || git clone git@github.com:memnoth/unix-v6.git $@
 
+dependency/simh.trailing-edge.com/uv6swre: dependency/simh.trailing-edge.com dependency/simh.trailing-edge.com/uv6swre.zip
+	unzip -o $</*.zip -d $@
+
+dependency/simh.trailing-edge.com/uv6swre.zip: dependency/simh.trailing-edge.com
+	curl -so $@ http://simh.trailing-edge.com/kits/uv6swre.zip
+
 dependency/simh: dependency
 	mkdir -p $@
 
 dependency/memnoth: dependency
+	mkdir -p $@
+
+dependency/simh.trailing-edge.com: dependency
 	mkdir -p $@
 
 dependency:
