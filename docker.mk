d92e5315a4cf21a404f5b6c573a22e068f93b507
tamakiii <tamakiii@users.noreply.github.com>
2023-06-26 16:29:17 +0900

    using $(call)

--- docker.mk
+++ docker.mk
@@ -1,7 +1,9 @@
-.PHONY: help build bash clean
+.PHONY: help build bash pd11 clean
 
 IMAGE := tamakiii-sandbox/reading-unix-v6
 TAG := latest
+
+run = docker run -it --rm -v /tmp -w /tmp $(IMAGE):$(TAG) $1
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -13,7 +15,10 @@ build: Dockerfile
 	docker build -t $(IMAGE):$(TAG) .
 
 bash:
-	docker run -it --rm -v /tmp -w /tmp $(IMAGE):$(TAG) $@
+	$(call run, $@)
+
+pd11:
+	$(call run, $@)
 
 teardown:
 	$(MAKE) -f dependency.mk clean
